9f89b4bb038a00ca8fd077a24276e5738df028d2
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-04-20 17:41:14 -0400

    refactor(grid-coloring): add check to avoid styling div container; wrote function call to create base-case (grid size = 1)

--- code.js
+++ code.js
@@ -10,13 +10,13 @@ const button = document.querySelector("button");
 const createGrid = function(gridSize){
     for (let rowNum = 1; rowNum <= gridSize; rowNum++){
         for (let colNum = 1; colNum <= gridSize; colNum++){
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("id", rowNum + "," + colNum);
-        newDiv.setAttribute("class", "item");
-        gridContainerRef.appendChild(newDiv);
+            const newDiv = document.createElement("div");
+            newDiv.setAttribute("id", rowNum + "," + colNum);
+            newDiv.setAttribute("class", "item");
+            gridContainerRef.appendChild(newDiv);
+        }
     }
-}
-
+    
 }
 
 
@@ -49,11 +49,15 @@ button.addEventListener("click", e => {
 
 // adding event listener that changes grid color when user hovers over item
 gridContainerRef.addEventListener("mouseover", e => {
-
-    //identifying item hovered over
     const hoveredItem = e.target;
-
-    //changing color
-    hoveredItem.style.backgroundColor = "blue";
+    if (hoveredItem !== gridContainerRef){
+        hoveredItem.style.backgroundColor = "blue";
+    }
 })
+
+
+
+
+// adding initial case (grid = 1 child div)
+createGrid(1);
 
