64870d5a43ed585a83f8e45a15089b2f66941255
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-04-20 17:35:33 -0400

    refactor(grid-creation): create function to generate grid based on input size

--- code.js
+++ code.js
@@ -1,12 +1,15 @@
 
-// defining constants
+// initializing references
 const gridContainerRef = document.querySelector("#grid-container");
-const MAX_ROWS = 16;
-const MAX_COLS = 16;
+const button = document.querySelector("button");
 
-// creating new divs (according to MAX_ROWS and MAX_COLS)
-for (let rowNum = 1; rowNum <= MAX_ROWS; rowNum++){
-    for (let colNum = 1; colNum <= MAX_COLS; colNum++){
+
+
+
+// function to create grid html divs
+const createGrid = function(gridSize){
+    for (let rowNum = 1; rowNum <= gridSize; rowNum++){
+        for (let colNum = 1; colNum <= gridSize; colNum++){
         const newDiv = document.createElement("div");
         newDiv.setAttribute("id", rowNum + "," + colNum);
         newDiv.setAttribute("class", "item");
