a4baea957c4ae7fb4ff5e793e1d06ba9a62d33de
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-04-20 16:50:11 -0400

    feat(event-listener): add event listener to change grid color on "mouseover"

--- code.js
+++ code.js
@@ -13,3 +13,14 @@ for (let rowNum = 1; rowNum <= MAX_ROWS; rowNum++){
         gridContainerRef.appendChild(newDiv);
     }
 }
+
+// adding event listener that changes grid color when user hovers over item
+gridContainerRef.addEventListener("mouseover", e => {
+
+    //identifying item hovered over
+    const hoveredItem = e.target;
+
+    //changing color
+    hoveredItem.style.backgroundColor = "blue";
+})
+
